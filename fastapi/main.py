6658c141ddaaef0a11f08fe5e4e2bf01033c5389
ab.land <ablandaeta@gmail.com>
2025-12-06 00:33:37 +0100

    refactor: change the Hashing lib to pwdlib as FastAPI docs recommend

--- fastapi/main.py
+++ fastapi/main.py
@@ -1,16 +1,20 @@
-import sys
-import os
-import bcrypt #change for pwdlib as recommended in the fastapi docs
-
 from fastapi import FastAPI, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 from sqlalchemy.exc import IntegrityError
+from pwdlib import PasswordHash
 from typing import List
 
 from db.database import SessionLocal, User
-from . import schemas
+import schemas
+
+password_hash = PasswordHash.recommended()
 
 app = FastAPI(title="FastAPI Backend Service")
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return password_hash.verify(plain_password, hashed_password)
+def hash_password(plain_password: str) -> str:
+    return password_hash.hash(plain_password)
 
 def get_db():
     db = SessionLocal()
@@ -19,13 +23,17 @@ def get_db():
     finally:
         db.close()
 
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
 @app.post('/v1/users/', response_model=schemas.UserResponse, status_code=status.HTTP_201_CREATED)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
-    hashed_password = bcrypt.hashpw(user.password.encode('utf-8'), bcrypt.gensalt())
+    hashed_password = hash_password(user.password)
     
     db_user = User(name=user.username, 
                    email=user.email, 
-                   hashed_password=hashed_password.decode('utf-8'))
+                   hashed_password=hashed_password)
     try:
         db.add(db_user)
         db.commit()
